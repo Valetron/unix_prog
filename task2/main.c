6e8abad7997744187d56a7a9f8edd361fec03274
Valetron <notspammeplz@gmail.com>
2023-04-03 23:27:18 +0300

    Обработчик сигнала

--- task2/main.c
+++ task2/main.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include <signal.h>
 
+int num;
+
+void handler(int sig)
+{
+    FILE* file = fopen("file.txt", "wb");
+    char strNum[1];
+
+    puts("Writing a number...");
+
+    fwrite(&num, sizeof(int), 1, file);
+    fclose(file);
+
+    exit(0);
+}
+
 int main(int argc, char** argv)
 {
-    printf("ppid - %d\n", getpid());
+    signal(SIGINT, handler);
 
-    signal(SIGINT, SIG_IGN);
+    FILE* file = fopen("file.txt", "rb");
+    // fscanf(file, "%d", &num);
+    fread(&num, sizeof(int), 1, file);
+    printf("last number - %d\n", num);
+    fclose(file);
+
 
     while (1)
     {
-        puts("kill me");
+        printf("%d\n", num++);
+        sleep(1);
     }
 
     return 0;
